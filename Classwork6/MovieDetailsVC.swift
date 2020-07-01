98197de0e035c7f969ad3a1a395138e780b11132
جنى مسيلم الظفيري124 <janamusailem2@gmail.com>
2020-07-02 02:48:23 +0300

    done with the whole class work

---
+++ Classwork6/MovieDetailsVC.swift
@@ -0,0 +1,62 @@
+//
+//  MovieDetailsVC.swift
+//  Classwork6
+//
+//  Created by janoya elba6a on 7/2/20.
+//  Copyright © 2020 Hasan Alsaffar. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsVC: UIViewController {
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieRatingLabel: UILabel!
+    @IBOutlet weak var movieRatedLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    
+    @IBOutlet weak var actorImageView1: UIImageView!
+    @IBOutlet weak var actorLabel1: UILabel!
+    @IBOutlet weak var actorImageView2: UIImageView!
+    @IBOutlet weak var actorLabel2: UILabel!
+    @IBOutlet weak var actorImageView3: UIImageView!
+    @IBOutlet weak var actorLabel3: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       setMovieDetails()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func setMovieDetails(){
+        movieImageView.image = UIImage(named: selectedMovie.movieName)
+        
+        movieRatingLabel.text = "\(selectedMovie.rating)"
+        movieRatedLabel.text = selectedMovie.pgRating
+        releaseDateLabel.text = "\(selectedMovie.movieReleaseDate)"
+        
+        
+        actorImageView1.image = UIImage(named: selectedMovie.actors[0])
+        actorImageView2.image = UIImage(named: selectedMovie.actors[1])
+        actorImageView3.image = UIImage(named: selectedMovie.actors[2])
+        
+        
+        actorLabel1.text = selectedMovie.actors[0]
+        actorLabel2.text = selectedMovie.actors[1]
+        actorLabel3.text = selectedMovie.actors[2]
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
